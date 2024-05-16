42f8eb190c7ede3c60a6e2208ce8dc5c5703c011
FebruArdiansyah <februardiansyah68@gmail.com>
2024-05-16 21:54:09 +0700

    yang terbaruu

--- assets/js/main.js
+++ assets/js/main.js
@@ -175,32 +175,35 @@
    * Menu isotope and filter
    */
   window.addEventListener('load', () => {
-    let menuContainer = select('.menu-container');
+    let menuContainer = document.querySelector('.menu-container');
     if (menuContainer) {
       let menuIsotope = new Isotope(menuContainer, {
         itemSelector: '.menu-item',
         layoutMode: 'fitRows'
       });
-
-      let menuFilters = select('#menu-flters li', true);
-
-      on('click', '#menu-flters li', function(e) {
-        e.preventDefault();
-        menuFilters.forEach(function(el) {
-          el.classList.remove('filter-active');
+  
+      let menuFilters = document.querySelectorAll('#menu-flters li');
+  
+      menuFilters.forEach(filter => {
+        filter.addEventListener('click', function(e) {
+          e.preventDefault();
+          menuFilters.forEach(el => {
+            el.classList.remove('filter-active');
+          });
+          this.classList.add('filter-active');
+  
+          menuIsotope.arrange({
+            filter: this.getAttribute('data-filter')
+          });
+          menuIsotope.on('arrangeComplete', function() {
+            AOS.refresh();
+          });
         });
-        this.classList.add('filter-active');
-
-        menuIsotope.arrange({
-          filter: this.getAttribute('data-filter')
-        });
-        menuIsotope.on('arrangeComplete', function() {
-          AOS.refresh()
-        });
-      }, true);
+      });
     }
-
   });
+  
+  
 
   /**
    * Initiate glightbox 
